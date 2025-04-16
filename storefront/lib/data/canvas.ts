3972b04b82be5e8f861b64c0584a3aa3edf712b4
Victor Gerbrands <victorgerbrands@gmail.com>
2025-04-16 16:14:06 +0200

    fix: canvas url

--- storefront/lib/data/canvas.ts
+++ storefront/lib/data/canvas.ts
@@ -10,7 +10,9 @@ import {
 import medusaError from "../util/medusa-error";
 import { Canvas } from "@/types";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  "https://" + process.env.VERCEL_PROJECT_PRODUCTION_URL ||
+  "http://localhost:3000";
 
 export async function addToCanvas(input: Partial<Canvas>) {
   const canvasId = await getCanvasId();

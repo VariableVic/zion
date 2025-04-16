1f3307fe4c3e6d8f27f8a55270d485c9e3fab958
Victor Gerbrands <victorgerbrands@gmail.com>
2025-04-16 15:30:26 +0200

    fix: app router

--- storefront/app/page.tsx
+++ storefront/app/page.tsx
@@ -2,17 +2,11 @@ import { Suspense } from "react";
 import { Storefront } from "@/components/storefront";
 import { StorefrontSkeleton } from "@/components/storefront-skeleton";
 
-export default async function Home({
-  params,
-}: {
-  params: Promise<{ path: string[] }>;
-}) {
-  const { path } = await params;
-
+export default async function Home() {
   return (
     <main className="flex h-full min-h-screen flex-col">
       <Suspense fallback={<StorefrontSkeleton />}>
-        <Storefront path={path} />
+        <Storefront />
       </Suspense>
     </main>
   );
